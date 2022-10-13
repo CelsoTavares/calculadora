aec972de54a2947cae6d51b0d7a292ab6c3b74aa
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-10-13 14:37:51 -0300

    add

--- script/script.js
+++ script/script.js
@@ -1,18 +1,21 @@
 let printed = document.querySelector(".calculator")
 
  function calc(e){
- console.log(printed.innerHTML.length)
  printed.innerHTML += e
   if(e === 'C'){
     printed.innerHTML = ''
   
   }else if(e === ''){
     printed.innerHTML = printed.innerHTML.substring(0, printed.innerHTML.length - 1)
+  
+  }else if(printed.innerHTML.includes('**') || printed.innerHTML.includes('..') || printed.innerHTML.includes('//') || printed.innerHTML.includes('%%') || printed.innerHTML.includes('--') || printed.innerHTML.includes('++')){
+    printed.innerHTML = printed.innerHTML.substring(0, printed.innerHTML.length - 1)
+  
+  }else if(printed.innerHTML === '%' || printed.innerHTML === '*' || printed.innerHTML === '/'){
+    printed.innerHTML = '';
   }
 }
 function result(){
-  
-  printed.innerHTML = eval(printed.innerHTML)
- 
+   printed.innerHTML = eval(printed.innerHTML)
  }
 
