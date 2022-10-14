336f9183ab23f7b2849763d8365e4c35f7ed07bb
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-10-13 23:15:34 -0300

    format

--- script/script.js
+++ script/script.js
@@ -1,17 +1,47 @@
 let printed = document.querySelector(".calculator")
 
  function calc(e){
- printed.innerHTML += e
-  if(e === 'C'){
+  
+   printed.innerHTML += e
+  
+   if(e === 'AC'){
     printed.innerHTML = ''
   
+  } else if(printed.innerHTML.includes('**') || 
+            printed.innerHTML.includes('..') || 
+            printed.innerHTML.includes('//') || 
+            printed.innerHTML.includes('%%') || 
+            printed.innerHTML.includes('--') || 
+            printed.innerHTML.includes('++') ||
+            printed.innerHTML.includes('*/') || 
+            printed.innerHTML.includes('*%') || 
+            printed.innerHTML.includes('+*') || 
+            printed.innerHTML.includes('+-') || 
+            printed.innerHTML.includes('+/') || 
+            printed.innerHTML.includes('+%') || 
+            printed.innerHTML.includes('-+') || 
+            printed.innerHTML.includes('-*') || 
+            printed.innerHTML.includes('-/') || 
+            printed.innerHTML.includes('-%') || 
+            printed.innerHTML.includes('/*') || 
+            printed.innerHTML.includes('%*') || 
+            printed.innerHTML.includes('%/') || 
+            printed.innerHTML.includes('/%') || 
+            printed.innerHTML.includes('.+') || 
+            printed.innerHTML.includes('.-') || 
+            printed.innerHTML.includes('./') || 
+            printed.innerHTML.includes('.%')){
+  
+                printed.innerHTML = printed.innerHTML.slice(0, - 1)
+                
   }else if(e === ''){
-    printed.innerHTML = printed.innerHTML.substring(0, printed.innerHTML.length - 1)
   
-  }else if(printed.innerHTML.includes('**') || printed.innerHTML.includes('..') || printed.innerHTML.includes('//') || printed.innerHTML.includes('%%') || printed.innerHTML.includes('--') || printed.innerHTML.includes('++')){
-    printed.innerHTML = printed.innerHTML.substring(0, printed.innerHTML.length - 1)
+    printed.innerHTML = printed.innerHTML.slice(0, - 1)
   
-  }else if(printed.innerHTML === '%' || printed.innerHTML === '*' || printed.innerHTML === '/'){
+  }else if(printed.innerHTML === '%' || 
+  printed.innerHTML === '*' || 
+  printed.innerHTML === '/'){
+  
     printed.innerHTML = '';
   }
 }
